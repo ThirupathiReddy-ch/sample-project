9ef3b7f9fc0711e0989c1a934e742af1e8507b63
ThirupathiReddy-ch <164163961+ThirupathiReddy-ch@users.noreply.github.com>
2025-09-03 16:19:09 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,19 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class DemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Spring Boot app is running!";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
     }
 }
 
